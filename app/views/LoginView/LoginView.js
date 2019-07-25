c7e19e2135f1241efec1b01053176898a3a59bc1
YuriiStetsula <s.yuri@ukr.net>
2019-07-25 22:00:39 +0300

    App: add signup and login views

--- app/views/LoginView/LoginView.js
+++ app/views/LoginView/LoginView.js
@@ -1,15 +1,16 @@
 import React from 'react'
+import  { Text, View, TextInput, StyleSheet, Button, TouchableOpacity } from 'react-native'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 
 import * as AuthActions from '../../actions/auth'
 import * as RequestActions from '../../actions/request'
 
-const mapDispatchToProps = dispatch => {
+function mapDispatchToProps (dispatch) {
   return bindActionCreators({ ...AuthActions, ...RequestActions }, dispatch)
 }
 
-const mapStateToProps = state => {
+function mapStateToProps (state) {
   return {
     request: state.request.firebasesignin,
     token: state.user.token
@@ -18,10 +19,79 @@ const mapStateToProps = state => {
 
 class LoginView extends React.Component {
 
+
+  state = {
+    email: '',
+    password: ''
+  }
+
+  onLoginPress = () => {
+    const { email, password } = this.state
+    const { signIn, resetRequest } = this.props
+    signIn({ email, password })
+    .then(() => { resetRequest('firebasesignin') })
+  }
+
+  onSignupPress = () => {
+    console.log('login pressed')
+  }
+
+  onEmailChange = value => {
+    this.setState({email: value})
+  }
+
+  onPasswordChange = value => {
+    this.setState({password: value})
+  }
+
   render () {
-    return null
+    const { request } = this.props
+    const { email, password } = this.state
+    const disabled = request.fetching || !email || !password
+
+    return <View style={styles.container}> 
+      <TextInput
+        value={this.state.email}
+        style={styles.border}
+        onChangeText={this.onEmailChange}
+        placeholder='email'
+      />
+      <TextInput
+        style={styles.border}
+        value={this.state.password}
+        onChangeText={this.onPasswordChange}
+        placeholder='password'
+        secureTextEntry
+      />
+      <Button
+        disabled={disabled}
+        title='Login'
+        onPress={this.onLoginPress}
+      />
+      <Text>OR</Text>
+      <TouchableOpacity disabled={request.fetching} onPress={this.onSignupPress}>
+        <Text>Signup</Text>
+      </TouchableOpacity>
+    </View>
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  border: {
+    width: '85%',
+    margin: 10,
+    padding: 15,
+    fontSize: 16,
+    borderColor: '#d3d3d3',
+    borderBottomWidth: 1,
+    textAlign: 'center'
+  }
+})
 
 const ConnectedLoginView = connect(mapStateToProps, mapDispatchToProps)(LoginView)
 export { ConnectedLoginView as LoginView }
--- app/views/SignupView/SignupView.js
+++ app/views/SignupView/SignupView.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Text, TextInput, View, StyleSheet, Button, TouchableOpacity } from 'react-native'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 import * as AuthActions from '../../actions/auth'
@@ -14,12 +15,95 @@ function mapStateToProps (state) {
   }
 }
 
+
 class SignupView extends React.Component {
 
+  state = {
+    email: '',
+    password: '',
+    userName: ''
+  }
+
+  onSignupPress = () => {
+    const { email, userName, password } = this.state
+    const { signUp, resetRequest } = this.props
+    signUp({ email, userName, password })
+      .then(() => { resetRequest('signup') })
+      .then(() => { resetRequest('firebasesignin') })
+  }
+
+  onEmailChange = value => {
+    this.setState({ email: value })
+  }
+
+  onUserNameChange = value => {
+    this.setState({ userName: value })
+  }
+
+  onPasswordChange = value => {
+    this.setState({ password: value })
+  }
+
+  onLoginPress = () => {
+    console.log('signup pressed')
+  }
+
   render () {
-    return null
+    const { request } = this.props
+    const { email, password, userName } = this.state
+    const disabled = request.fetching || !email || !password || !userName
+    return <View style={styles.container}>
+      <TextInput
+        style={styles.border}
+        value={email}
+        onChangeText={this.onEmailChange}
+        placeholder='email'
+      />
+      <TextInput
+        style={styles.border}
+        value={userName}
+        onChangeText={this.onUserNameChange}
+        placeholder='username'
+      />
+      <TextInput
+        style={styles.border}
+        value={password}
+        onChangeText={this.onPasswordChange}
+        placeholder='password'
+        secureTextEntry
+      />
+      <Button
+        title='Signup'
+        disabled={disabled}
+        onPress={this.onSignupPress}
+      />
+      <Text>OR</Text>
+      <TouchableOpacity disabled={request.fetching} onPress={this.onLoginPress}>
+        <Text>Login</Text>
+      </TouchableOpacity>
+    </View>
   }
 }
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  border: {
+    width: '85%',
+    margin: 10,
+    padding: 15,
+    fontSize: 16,
+    borderColor: '#d3d3d3',
+    borderBottomWidth: 1,
+    textAlign: 'center'
+  }
+})
+
+
+
 const ConnectedSignupView = connect(mapStateToProps, mapDispatchToProps)(SignupView)
+export { ConnectedSignupView as SignupView }
 export default ConnectedSignupView
