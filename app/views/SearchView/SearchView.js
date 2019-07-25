e833da852e2d2f336b338467814f1792d23e52dd
YuriiStetsula <s.yuri@ukr.net>
2019-07-26 02:03:59 +0300

    SearchView: add ScrollView

--- app/views/SearchView/SearchView.js
+++ app/views/SearchView/SearchView.js
@@ -1,7 +1,9 @@
-import React from 'react'
+import React from 'react';
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
+import { View, Text, SafeAreaView, TextInput, Image, TouchableOpacity, StyleSheet, ScrollView } from 'react-native';
 import { searchUser, getUser } from '../../actions/user'
+import { NavigationEvents } from 'react-navigation'
 
 const mapDispatchToProps = (dispatch) => {
   return bindActionCreators({ searchUser, getUser }, dispatch)
@@ -13,12 +15,126 @@ const mapStateToProps = (state) => {
   }
 }
 
-class SearchView extends React.Component {
 
-  render () {
-    return null
+// const fakeUser = {
+//   email:"yurii@test.com",
+//   followers: null,
+//   following: null,
+//   photo:"https://image.flaticon.com/icons/png/512/55/55089.png",
+//   uid:"HW5gQGTUQvXcfi9OLwasb1Ok6kt2",
+//   userName:"yuriistetsula",
+// }
+
+
+// const fakeUsers = []
+// for (let i = 0; i < 999; i++) {
+//   fakeUsers.push({ ...fakeUser, uid: i})
+// }
+
+class Search extends React.Component {
+	state = {
+    search: '',
+    users: []
+  }
+
+
+  // onWillFocus = () => {
+  //   console.log('onWillFocus')
+  //   this.setState({
+  //     users: fakeUsers
+  //   })
+  // }
+
+	searchUser = () => {
+    this.props.searchUser(this.state.search)
+     .then(data => {
+       if (data && !data.error) {
+         this.setState({users: data.response.data})
+       }
+     }) 
+	}
+
+	goToUser = (user) => {
+    this.props.getUser(user.uid)
+    .then(data => {
+      if (data && !data.error) {
+        this.props.navigation.navigate('User', { user: data.response }) 
+      }
+    })
+	}
+
+  render() {
+    return (
+      <View style={styles.container}>
+        {/* <NavigationEvents onWillFocus={this.onWillFocus} /> */}
+
+	      <TextInput
+	        style={styles.input}
+	        onChangeText={(search) => this.setState({search})}
+	        value={this.state.search}
+	        returnKeyType='send'
+          placeholder='Search'
+          onSubmitEditing={this.searchUser}/>
+          <ScrollView> 
+            {
+              this.state.users.map(item => {
+                return <TouchableOpacity key={item.uid} onPress={() => this.goToUser(item)} style={[styles.row, styles.space]}>
+                  <Image style={styles.roundImage} source={{uri: item.photo}}/>
+                  <View style={[styles.container, styles.left]}>
+                    <Text style={styles.bold}>{item.userName}</Text>
+                    <Text style={styles.gray}>{item.bio}</Text>
+                  </View>
+                </TouchableOpacity>
+              })
+            }
+          </ScrollView>
+      </View>
+    )
   }
 }
 
-const ConnetedSearchView = connect(mapStateToProps, mapDispatchToProps)(SearchView)
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  }, 
+  input: {
+    width: '90%',
+    margin: 15,
+    padding: 15,
+    alignSelf: 'center',
+    borderColor: '#d3d3d3',
+    borderWidth: 1,
+    borderRadius: 50,
+    fontSize: 16,  
+  },
+  row: {
+    flexDirection: 'row'
+  },
+  space: {
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  roundImage: {
+    width: 40, 
+    height: 40,
+    borderRadius: 20,
+    margin: 10,
+    backgroundColor: '#adadad'
+  },
+  bold: {
+    fontWeight: 'bold',
+  },
+  gray: {
+    color: '#adadad',
+  },
+  left: {
+    alignItems: 'flex-start',
+  },
+  space: {
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+})
+
+const ConnetedSearchView = connect(mapStateToProps, mapDispatchToProps)(Search)
 export default ConnetedSearchView
